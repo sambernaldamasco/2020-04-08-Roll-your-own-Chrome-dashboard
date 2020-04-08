8d2ae1908dc0faf67450148ed8bde39f1a70a2f3
Sam Bernal <sambernaldamasco@gmail.com>
2020-04-08 13:27:00 -0400

    decided to go for create-react-app

---
+++ todo-list/build/precache-manifest.cf4d7ea2f33fbdd6a56dc66ec79eeceb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09445f99934a3a753b46df62aa19f16f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1f54af55f46d3117d63",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a29f0c7f5b1184821fba",
+    "url": "/static/js/2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a1b24e857560f5c2c37",
+    "url": "/static/js/app.js"
+  },
+  {
+    "revision": "f1f54af55f46d3117d63",
+    "url": "/static/js/main.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
