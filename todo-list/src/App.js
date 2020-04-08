40047c7b435962957fe0e08f1ed3003cffbd935b
Sam Bernal <sambernaldamasco@gmail.com>
2020-04-08 13:52:40 -0400

    displaying todos

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
+	const testTodos = [
+		{ title: 'testest', description: 'one test', priority: 'medium' },
+		{ title: 'testest', description: 'two test', priority: 'medium' },
+	];
 	return (
 		<div className="App">
 			<TodoForm />
+			<TodoList tasks={testTodos} />
 		</div>
 	);
 }
--- todo-list/src/components/TodoList.js
+++ todo-list/src/components/TodoList.js
@@ -1,1 +1,19 @@
 import React, { useState } from 'react';
+
+const TodoList = (props) => {
+	return (
+		<>
+			{props.tasks.map((task) => {
+				return (
+					<div>
+						<h2> {task.title}</h2>
+						<p> {task.description}</p>
+						<h4> {task.priority}</h4>
+					</div>
+				);
+			})}
+		</>
+	);
+};
+
+export default TodoList;
